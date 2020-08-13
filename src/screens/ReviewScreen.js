dfd8110ea388437451469e5195fbac820e83cbdd
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-13 02:07:07 -0700

    Added styling for cards on review screen

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect } from 'react'
-import { StyleSheet, Text, View, Platform, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Platform, ScrollView, Linking } from 'react-native'
 import { Card, Button } from 'react-native-elements'
 import { Context as LikedJobsContext } from '../context/LikedJobsContext'
+import MapView from 'react-native-maps'
 
 const ReviewScreen = ({ navigation }) => {
 
@@ -9,13 +10,41 @@ const ReviewScreen = ({ navigation }) => {
     
     const renderLikedJobs = () => {
         return state.map(job => {
+
+            const { 
+                company, 
+                formattedRelativeTime, 
+                url, 
+                latitude, 
+                longitude,
+                jobtitle,
+                jobkey 
+            } = job
+            const initialRegion = {
+                longitude: longitude,
+                latitude: latitude,
+                latitudeDelta: 0.045,
+                longitudeDelta: 0.02
+            }
+
             return (
-                <Card>
+                <Card title={jobtitle} key={jobkey}>
                     <View style={{ height: 200 }}>
+                        <MapView 
+                            style={{ flex: 1}}
+                            cacheEnabled={true}
+                            scrollEnabled={false}
+                            initialRegion={initialRegion}
+                        />
                         <View style={styles.detailWrapper}>
-                            <Text style={styles.italics}>{job.company}</Text>
-                            <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+                            <Text style={styles.italics}>{company}</Text>
+                            <Text style={styles.italics}>{formattedRelativeTime}</Text>
                         </View>
+                        <Button 
+                            title="Apply Now!"
+                            buttonStyle={{ backgroundColor: "#03A9F4" }}
+                            onPress={() => Linking.openURL(url)}
+                        />
                     </View>
                 </Card>
             )
@@ -54,7 +83,8 @@ const styles = StyleSheet.create({
     detailWrapper: {
         marginBottom: 10,
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        marginTop: 10
     },
     italics: {
         fontStyle: 'italic'
